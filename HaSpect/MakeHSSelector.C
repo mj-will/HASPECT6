4cf1cef73150d5710beef15fbd249e45e4fd2a37
Peter Pauli <p.pauli.1@research.gla.ac.uk>
2017-10-04 16:22:50 +0100

    Add an example of how to use the THSSkeleton class to make a project class and a THSSelector

---
+++ HaSpect/MakeHSSelector.C
@@ -0,0 +1,30 @@
+/** \file MakeHSSelector.C
+ * 
+ * Macro to generate a TSelector
+ * using the THSSkeleton class
+ * 
+ * Usage:
+ * root -l \<file containing tree> \--hssel '$HSANA/MakeHSSelector.C(\<treename>)'
+ * 
+ */
+
+void MakeHSSelector(TString treeName = "tree"){
+	
+	THSSkeleton* sk = new THSSkeleton();
+	
+	sk->SetHisto();   //Going to make histograms
+	sk->SetNewTree();   //Going to make new tree
+	
+	sk->SetLPS(3); //Add longitudinal phase space class
+	
+	sk->SetFinalState("pKK");
+	
+	sk->SetFinalStateTopo("pKK"); //add topology where all three particles are detected
+	sk->SetFinalStateParts("Proton:proton,KPlus:K+,KMinus:K-"); //set final state particles <name:pdgname>
+	sk->CreateMyFinalState(); //create a project class
+	
+	sk->SetFinalState("THSpKK"); //CreateMyFinalState() adds a THS in front of the name. To use the same class directly reset fFinalName here.
+	
+	sk->CreateSelector("THSSelector_vh",_file0->GetName(),treeName,""); //create selector with all specifications
+	
+}
