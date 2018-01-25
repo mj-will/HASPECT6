2f3e00e3db1139099faf3f07a7ed401ab59ee066
dglazier <Derek.Glazier@glasgow.ac.uk>
2018-01-25 15:30:58 +0000

    THSHipoTrigger fix

--- HaSpect/THSHipoTrigger.C
+++ HaSpect/THSHipoTrigger.C
@@ -20,7 +20,7 @@ Bool_t THSHipoTrigger::ReadEvent(Long64_t entry){
  
   if(!THSHipoReader::ReadEvent()) return kFALSE;
 
-  CreateBitPattern(fRunTrig->val());
+  CreateBitPattern(fRunTrig->Val());
   fFTHigh=fTrigBits[30];
   fFTLow=fTrigBits[29];
   return kTRUE;
@@ -32,12 +32,11 @@ void  THSHipoTrigger::CreateBitPattern(long val)
    unsigned int mask = 1 << (sizeof(int) * 8 - 1);
 
    Int_t Nbits=sizeof(int) * 8;
-   Int_t bits[sizeof(int) * 8];
    
-   for(int i = 0; i < sizeof(int) * 8; i++){
+   for(int i = 0; i <Nbits; i++){
      
      if( (val & mask) == 0 ){
-       fTrigBits[fNbits-i-1]=0;
+       fTrigBits[Nbits-i-1]=0;
      }
      else{
        fTrigBits[Nbits-i-1]=1;    
