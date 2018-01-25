bf531ea825f5a17247fcd9cab860d78a3c7e9f8e
dglazier <Derek.Glazier@glasgow.ac.uk>
2018-01-25 20:52:39 +0000

    THSHipoTrigger Add REC::Event

--- HaSpect/THSHipoTrigger.C
+++ HaSpect/THSHipoTrigger.C
@@ -1,29 +1,82 @@
 #include "THSHipoTrigger.h"
 
 THSHipoTrigger::THSHipoTrigger(){
-   
-  fHipo->ConfigBank("RUN::config");
-  fRunConBank=fHipo->GetBank("Run::config");
-
-  fRunTrig=fRunConBank->GetItem("trigger");
+ 
+  
 }
+Bool_t THSHipoReader::Init(TString filename,TString name){
+
+  THSHipoReader::Init(filename,name);
+  
+  if(!fRecEvBank){
+    Info("THSHipoTrigger","Opened file");
+    fHipo->ConfigOnlyBank("REC::Event");
+    fHipo->ConfigOnlyBank("RUN::config");
+    fHipo->ConfigBanks();
+    
+    fRecEvBank=fHipo->GetBank("REC::Event");
+    fRunConBank=fHipo->GetBank("RUN::config");
+
+    fRecEvNRun=fRecEvBank->GetItem("NRUN");
+    fRecEvNEVENT=fRecEvBank->GetItem("NEVENT");
+    fRecEvTYPE=fRecEvBank->GetItem("TYPE");
+    fRecEvTRG=fRecEvBank->GetItem("TRG");
+    fRecEvHelic=fRecEvBank->GetItem("Helic");
+    fRecEvEVNTime=fRecEvBank->GetItem("EVNTime");
+    fRecEvBCG=fRecEvBank->GetItem("BCG");
+    fRecEvLT=fRecEvBank->GetItem("LT");
+    fRecEvSTTime=fRecEvBank->GetItem("STTime");
+    fRecEvRFTime=fRecEvBank->GetItem("RFTime");
+    fRecEvPTIME=fRecEvBank->GetItem("PTIME");
+
+    
+    fRunTrig=fRunConBank->GetItem("trigger");
+  }
+  return kTRUE;
+}
+
 void THSHipoTrigger::InitOutput(TString filename){
   THSDataManager::InitOutput(filename);
   fWriteTree->Branch("FTHigh",&fFTHigh,"FTHigh/I");
   fWriteTree->Branch("FTLow",&fFTLow,"FTLow/I");
+
+
+  fWriteTree->Branch("NRun",&fNRun,"NRun/I");
+  fWriteTree->Branch("NEvent",&fNEvent,"NEvent/I");
+  fWriteTree->Branch("EvTime",&fEvTime,"EvTime/F");
+  fWriteTree->Branch("Type",&fType,"Type/I");
+  fWriteTree->Branch("Trig",&fTrig,"Trig/I");
+  fWriteTree->Branch("BCG",&fBCG,"BCG/F");
+  fWriteTree->Branch("LT",&fLT,"LT/F");
+  fWriteTree->Branch("STTime",&fSTTime,"STTime/F");
+  fWriteTree->Branch("RFTime",&fRFTime,"RFTime/F");
+  fWriteTree->Branch("Helic",&fHelic,"Helic/I");
+  fWriteTree->Branch("PTime",&fPTime,"PTime/F");
+ 
  }
 
 Bool_t THSHipoTrigger::ReadEvent(Long64_t entry){
   /// initalize variables:
-  fFTHigh=0;
-  fFTLow=0;
- 
-  if(!THSHipoReader::ReadEvent()) return kFALSE;
+  fNRun=fRecEvNRun->Val();
+  fNEvent=fRecEvNEVENT->Val();
+  fType=fRecEvTYPE->Val();
+  fTrig=fRecEvTRG->Val();
+  fTrig=fRecEvHelic->Val();
+  fEvTime=fRecEvEVNTime->Val();
+  fBCG=fRecEvBCG->Val();
+  fLT=fRecEvLT->Val();
+  fSTTime=fRecEvSTTime->Val();
+  fRFTime=fRecEvRFTime->Val();
+  fPTime=fRecEvPTIME->Val();
 
   CreateBitPattern(fRunTrig->Val());
   fFTHigh=fTrigBits[30];
   fFTLow=fTrigBits[29];
-  return kTRUE;
+  
+  
+  THSHipoReader::ReadEvent();
+  return kTRUE;;
+
 }
 /////////////////////////////////////////////////////
 ///function to convert trigger bit pattern
--- HaSpect/THSHipoTrigger.h
+++ HaSpect/THSHipoTrigger.h
@@ -19,14 +19,40 @@ class THSHipoTrigger: public THSHipoReader{
 
   //banks
   THipoBank* fRunConBank=nullptr;
+  THipoBank* fRecEvBank=nullptr;
 
   //items in banks
   THipoItem* fRunTrig=nullptr;
- 
+  THipoItem* fRecEvNRun=nullptr;
+  THipoItem* fRecEvNEVENT=nullptr;
+  THipoItem* fRecEvTYPE=nullptr;
+  THipoItem* fRecEvTRG=nullptr;
+  THipoItem* fRecEvHelic=nullptr;
+  THipoItem* fRecEvEVNTime=nullptr;
+  THipoItem* fRecEvBCG=nullptr;
+  THipoItem* fRecEvLT=nullptr;
+  THipoItem* fRecEvSTTime=nullptr;
+  THipoItem* fRecEvRFTime=nullptr;
+  THipoItem* fRecEvPTIME=nullptr;
+  
   
   //Trigger variables
   Int_t fFTHigh=0; 
   Int_t fFTLow=0;
+
+  Int_t fNRun=0;//Run Number
+  Int_t fNEvent=0;;//Event Number
+  Float_t fEvTime=0;;//Event Time
+  Int_t fType=0;;//Event Type (Data or MC)
+  Int_t fTrig=0;;//Trigger Type (CLAS12_e-, FT_CLAS12_h,CLAS12_H,...) + Prescale Factor for that Trigger"}
+  Float_t fBCG=0;;//Faraday Cup Gated (Coulomb)
+  Float_t fLT=0;;//Clock
+  Float_t fSTTime=0;;//Event Start Time (ns)
+  Float_t fRFTime=0;;//RF Time (ns)
+  Int_t fHelic=0;;//Helicity of Event
+  Float_t PTime=0;;//Event Processing Time (UNIX Time = seconds)
+  
+
   
   Int_t fTrigBits[sizeof(int) * 8];
 
