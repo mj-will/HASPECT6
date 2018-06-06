e8094c984a09c59231c84a8be972fca02e51b62a
dglazier <Derek.Glazier@glasgow.ac.uk>
2018-06-06 11:41:19 +0100

    update createFinalState

--- Projects/createFinalState/CreateMyFinalState.C
+++ Projects/createFinalState/CreateMyFinalState.C
@@ -4,12 +4,12 @@
   //Give your project a name
   sk->SetFinalState("WillThisWork",kTRUE);  //creating the final state class 
   //Set the detected particle combinations you will analyse
-  sk->SetFinalStateTopo("pi+:pi-,pi+:pi-:pi0,proton:pi+:pi-:pi0");
+  sk->SetFinalStateTopo("");
   //Set the actual final state particles of the reaction
   //These will just be used as the data member names, for example
-  sk->SetFinalStateParts("Electron:e-,Proton:proton,Pip:pi+,Pim:pi-,Gamma1:gamma,Gamma2:gamma");
+  sk->SetFinalStateParts("");
    //define any parent particles that may have decayed, for example
-  sk->SetFinalStateParents("Pi0:pi0;Gamma1;Gamma2,Omega:omega;Pip;Pim;Pi0");
+  sk->SetFinalStateParents("");
  //Make some code
   sk->CreateMyFinalState();
 }
