0818936e0bc9bd762c84847bc9e5407d3bba710b
Michael Williams <michael.williams@zxilix.com>
2018-07-11 14:46:31 +0100

    Added code to setup variables

--- Projects/tmva/THSMVA.C
+++ Projects/tmva/THSMVA.C
@@ -6,8 +6,136 @@
 	This class inherits from TObject so can be saved to root file
 	and retrieved later
 */
+
+
 #include "THSMVA.h"
+//#include "isnan"
 
 ////////////////////////////////////////////////////////////
 /// Here I put Doxygen readable comments explaing what each function does
 
+THSMVA::THSMVA(){
+
+}
+
+/**
+ * Print the variables that have been set.
+ *
+ */
+
+void THSMVA::PrintVars(){
+    std::cout<<"Particles : "<<std::endl;
+    for (UInt_t i=0; i < particleNames.size(); i++){
+        std::cout<<"        "<< particleNames[i]<<std::endl;
+    }
+    std::cout<<"Particle propeties : "<<std::endl;
+    for (UInt_t i=0; i < particleProperties.size(); i++){
+        std::cout<<"                 "<< particleProperties[i]<<std::endl;
+    }
+    std::cout<<"Variables : "<<std::endl;
+    for (UInt_t i=0; i < variableNames.size(); i++){
+        std::cout<<"        "<< variableNames[i]<<std::endl;
+    }
+    std::cout<<"Number of variables : "<<nVars<<std::endl;
+}
+
+        
+
+
+/**
+ * Checks if a tree contains any NaNs and replaces them with zeros.
+ *
+ */
+
+void THSMVA::CheckTree(){
+    // number of variables
+    nVars = variableNames.size();
+    // loop over variables and replace NaNs
+    for (UInt_t i=0; i<fTrainTree->SetEntries(); i++) {
+        fTrainTree->GetEntry(i);
+        for (UInt_t ivar=0; ivar<nVars; ivar++) {
+            if (std::isnan(treevars[ivar])) {
+                vars[ivar] = 0;
+            }
+            else {
+                vars[ivar] = treevars[ivar];
+            }
+        }
+    }
+
+}
+
+/**
+ * Set variable names for topology 0
+ *
+ */
+
+void THSMVA::SetVariablesTopo_0(){
+    // Set branches in loop
+    particleNames = {"El", "P","Pip","Pim"};
+    variableNames = {};
+
+    for(auto const& pn: particleNames) {
+        for(auto const& pp: particleProperties) {
+            variableNames.push_back(pn+pp);
+        }
+    }
+    nVars = variableNames.size();
+}
+
+/**
+ * Set variable names for topology 1
+ *
+ */
+
+void THSMVA::SetVariablesTopo_1(){
+    // Set branches in loop
+    particleNames = {"El", "P","Pip"};
+    variableNames = {"NPerm", "NDet", "Detector"};
+
+
+    for(auto const& pn: particleNames) {
+        for(auto const& pp: particleProperties) {
+            variableNames.push_back(pn+pp);
+        }
+    }
+    nVars = variableNames.size();
+}
+
+/**
+ * Set variable names for topology 2
+ *
+ */
+
+void THSMVA::SetVariablesTopo_2(){
+    // Set branches in loop
+    particleNames = {"El", "P","Pim"};
+    variableNames = {"NPerm", "NDet", "Detector"};
+
+
+    for(auto const& pn: particleNames) {
+        for(auto const& pp: particleProperties) {
+            variableNames.push_back(pn+pp);
+        }
+    }
+    nVars = variableNames.size();
+}
+
+/**
+ * Set variable names for topology 3
+ *
+ */
+
+void THSMVA::SetVariablesTopo_3(){
+    // Set branches in loop
+    particleNames = {"El","Pip","Pim"};
+    variableNames = {"NPerm", "NDet", "Detector"};
+
+
+    for(auto const& pn: particleNames) {
+        for(auto const& pp: particleProperties) {
+            variableNames.push_back(pn+pp);
+        }
+    }
+    nVars = variableNames.size();
+}
--- Projects/tmva/THSMVA.h
+++ Projects/tmva/THSMVA.h
@@ -1,19 +1,42 @@
 #ifndef THSMVA_h
 #define THSMVA_h
 
+#include <iostream>
 #include <TObject.h>
-#include "TMVA/Tools.h"
+#include <TString.h>
+#include <TTree.h>
+#include <vector>
 
-using namespace TMVA;
+using namespace std;
+
+// TODO: consider making class for variable, parameter and properties
 
 class THSMVA : public TObject {
 
  public :
-  THSMVA()=default;
+  THSMVA();
   ~THSMVA()=default;
   
+  void PrintVars();
+  void CheckTree();
+  void SetVariablesTopo_0();
+  void SetVariablesTopo_1();
+  void SetVariablesTopo_2();
+  void SetVariablesTopo_3();
+  void SetTree(TTree* tree){fTrainTree = tree;};
+
  protected :
 
+  TTree* fTrainTree=nullptr;//!
+
+  // list of "properties"
+  std::vector<TString> particleProperties = {"Time","Edep","DeltaE", "PreE", "P", "Th", "Phi", "Vz", "TrChi2"};
+  std::vector<TString> particleNames;
+  std::vector<TString> variableNames;
+
+  Int_t nVars;
+  std::vector<Double_t> vars;
+  Float_t treevars[100];
 
  public:
   
